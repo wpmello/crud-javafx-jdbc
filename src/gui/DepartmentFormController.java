1ee4e0dbc275d15a6dc59f90fa952be15d21d097
wpmello <wagnermello903@gmail.com>
2021-09-09 19:29:11 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+
+	@FXML
+	private TextField txtId; 
+	
+	@FXML
+	private TextField txtxName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtNewAction");	
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		initialaziNodes();
+	}
+	
+	private void initialaziNodes() {
+		// método para restringir os 'TextFields'
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtxName, 30); 
+	}
+}
