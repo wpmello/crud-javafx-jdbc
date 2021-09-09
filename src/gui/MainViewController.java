09e84d0e427ea5b19acb4b77a7c04813141704e0
wpmello <wagnermello903@gmail.com>
2021-09-09 17:44:02 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,11 +36,14 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");	}
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentServices());
+			controller.updateTableView();
+		});	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml"); 
+		loadView("/gui/About.fxml", x -> {}); 
 	}
 	
 	
@@ -47,31 +51,9 @@ public class MainViewController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 	}
 
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) { 
 		
-		// método usado para carregar nova tela sobre a principal
-		
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();			
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // pegando e guardando o control 'VBox' da Scene principal' 
-			Node mainMenu = mainVBox.getChildren().get(0); 	// pegando e guardando 'MenuBar'
-			mainVBox.getChildren().clear();  // limpando 'MenuBar'
-			mainVBox.getChildren().add(mainMenu);	// adc o 'MenuBar' pela mainMenu
-			mainVBox.getChildren().addAll(newVBox);	// adc o novo 'MenuBar' pela newVBox
-		
-			// "synchronized" ajuda o método com multi threads, fazendo com q ele rode independente das threads q estão rolando
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-	
-		// foi feito uma cópia apenas a critério da aula. será arrumado no proximo commit
+		// foi acrecentado um 'Consumer' como parâmentro para q a função possa receber um lambda como argumento
 		
 		// método usado para carregar nova tela sobre a principal
 		
@@ -88,9 +70,9 @@ public class MainViewController implements Initializable {
 		
 			// "synchronized" ajuda o método com multi threads, fazendo com q ele rode independente das threads q estão rolando
 		
-			DepartmentListController controller = loader.getController(); // acessa controller da classe
-			controller.setDepartmentService(new DepartmentServices());
-			controller.updateTableView();
+			T controller = loader.getController(); // pegando o controller da classe
+			initializingAction.accept(controller); // ativando o controller
+		
 		}
 		catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
